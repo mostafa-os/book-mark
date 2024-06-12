18f7a3f2ea03c6f97ff251d6c174746e80f12829
msotafa-os <mostafaosama189@gmail.com>
2024-06-13 01:55:21 +0300

    edit

--- main.js
+++ main.js
@@ -28,3 +28,36 @@ ques.forEach((el) => {
       : el.classList.remove("h-fit");
   });
 });
+
+let form = document.querySelector("form"),
+  div = document.querySelector("form > div");
+(mail = document.querySelector("form input.mail")),
+  (icon = document.querySelector("form i")),
+  (error = document.querySelector(".error")),
+  (input = document.querySelector("form > input"));
+
+form.onsubmit = function (e) {
+  let re = /\w+@gmail.com/gi;
+  if (!re.test(mail.value) || mail.value === null) {
+    e.preventDefault();
+    if (window.innerWidth >= 640) {
+      div.style.transform = "translateY(13.5px)";
+    }
+    mail.style.borderColor = "hsl(0, 94%, 66%)";
+    icon.style.display = "inline";
+    error.style.display = "flex";
+  }
+};
+
+input.addEventListener("click", (e) => {
+  let re = /\w+@gmail.com/gi;
+  if (!re.test(mail.value) || mail.value === null) {
+    e.preventDefault();
+    if (window.innerWidth >= 640) {
+      div.style.transform = "translateY(13.5px)";
+    }
+    mail.style.borderColor = "hsl(0, 94%, 66%)";
+    icon.style.display = "inline";
+    error.style.display = "flex";
+  }
+});
